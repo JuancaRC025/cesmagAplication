9174f8d2f40df625fead9840f288749f8eb759ab
Juan <2.juan@gmail.com>
2023-10-25 09:32:31 -0500

    8vo

--- programas.php
+++ programas.php
@@ -1,3 +1,25 @@
 <?php
 include "index.php"
 ?>
+
+<h1>Tablas</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">codigo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Facultad</th>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>110</td>
+      <td>diseño Grafico</td>
+      <td>10</td>
+    </tr>
+    <a class="btn btn-success">Editar</a>
+    <a class="btn btn-danger">Eliminar</a>
+  </tbody>
+</table>
+</table>
